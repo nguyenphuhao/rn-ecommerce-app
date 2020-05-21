b0c6fd0fa65d68476eb948dc85ca9e58230a2ea5
Hao Nguyen <hao_nguyen@epam.com>
2020-05-21 16:13:43 +0700

    refresh token

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -1,10 +1,16 @@
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import AsyncStorage from '@react-native-community/async-storage';
 import {
   selectAuthToken,
   selectAuthLoading,
   selectAuthError,
 } from '../models/authentication/selectors';
-import { requestLogin } from '../models/authentication/actions';
+import {
+  requestLogin,
+  requestLoginSuccess,
+} from '../models/authentication/actions';
+import { AUTH_TOKEN } from '../constants';
 
 const useAuthentication = () => {
   const dispatch = useDispatch();
@@ -15,6 +21,23 @@ const useAuthentication = () => {
   const login = (username, password) => {
     dispatch(requestLogin(username, password));
   };
+
+  useEffect(() => {
+    const syncAuthToken = async () => {
+      const asyncAuthToken = await AsyncStorage.getItem(AUTH_TOKEN);
+      console.log('asyncAuthToken', asyncAuthToken);
+      if (asyncAuthToken) {
+        dispatch(requestLoginSuccess(asyncAuthToken));
+      }
+    };
+    syncAuthToken();
+  }, []);
+
+  useEffect(() => {
+    if (authToken) {
+      AsyncStorage.setItem(AUTH_TOKEN, authToken);
+    }
+  }, [authLoading]);
 
   return {
     login,
